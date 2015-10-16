73712a8efaabade0cf27548e8e652b4a9597962b
gadeta01 <gadeta01@luther.edu>
2015-10-16 15:19:11 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -1,16 +1,16 @@
 addItem = function() {
-  var list, newitem, selected, classnew; 
-  list = document.querySelector("#lista");
-  list.type = none;
-  newitem = document.createElement("li");
-  classnew = document.querySelector("#priority").value;
-  newitem.class = classnew;
-  box = document.createElement("input");
-  box.type = "checkbox"; 
-  taskdata = document.querySelector("#task").value;
-  tasknode = document.createTextNode(taskdata);
-  list.appendChild(newitem);
-  newitem.appendChild(box);
-  newitem.appendChild(tasknode);
-  
+  //var list, newitem, selected, classnew; 
+  //list = document.querySelector("#lista");
+  //list.type = none;
+  //newitem = document.createElement("li");
+  //classnew = document.querySelector("#priority").value;
+  //newitem.class = classnew;
+  //box = document.createElement("input");
+  //box.type = "checkbox"; 
+  //taskdata = document.querySelector("#task").value;
+  //tasknode = document.createTextNode(taskdata);
+  //list.appendChild(newitem);
+  //newitem.appendChild(box);
+  //newitem.appendChild(tasknode);
+  alert("The Button was clicked");
 }
